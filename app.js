2c385aec844cbd27148cea83426e11768336ba69
Luciano <luchogoma342@gmail.com>
2024-08-16 15:55:01 -0300

    Act ejercicio 24

--- app.js
+++ app.js
@@ -127,3 +127,118 @@ function invertirCadena(cadena){
 }
 
 console.log(invertirCadena("Hola"));
+
+// EJERCICIO 17
+function filtrarPorLongitud(a, b){
+    let nuevoArray = [];
+    a.forEach(element => {
+        if(typeof element === "string"){
+            if(element.length >= b){
+                nuevoArray.push(element);
+            }
+        }
+    });
+    return nuevoArray;
+}
+let arregloPalabras = ["Juan", "Longitud", "Palabra", "Mariano", "Auto", "casa", "Estupefaciente"];
+console.log(filtrarPorLongitud(arregloPalabras, 7));
+
+// EJERCICIO 18
+function Estudiante(nombre, edad, promedio){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.promedio = promedio;
+}
+
+let est1 = new Estudiante("Juan", 15, 8, 8.2);
+let est2 = new Estudiante("Mariano", 15, 9.3);
+let est3 = new Estudiante("Messi", 15, 7.6);
+let est4 = new Estudiante("Lupo", 15, 6.8);
+
+// EJERCICIO 19
+let arregloEstudiantes = [est1, est2, est3, est4];
+
+function buscarEstudiante(a, b){
+    let estudiante = "No encontrado";
+    a.forEach(element => {
+        if(element instanceof Estudiante){
+            if(typeof element.nombre === "string"){
+                if(element.nombre == b){
+                    estudiante = element;
+                }
+            }
+        }
+    });
+    return estudiante;
+}
+
+console.log(buscarEstudiante(arregloEstudiantes, "Juan"));
+
+// EJERCICIO 20
+function promedioClase(a){
+    let suma = 0;
+    let alumnos = 0;
+    a.forEach(element => {
+        if(element instanceof Estudiante){
+            if(typeof element.promedio === "number"){
+                suma += element.promedio;
+                alumnos++;
+            }
+        }
+    });
+    return (suma / alumnos);
+}
+
+console.log(promedioClase(arregloEstudiantes));
+
+// EJERCICIO 21
+function coche(marca, modelo, anio){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anio = anio;
+
+    this.arrancar = function(){
+        console.log("Arrancando...");
+    }
+    this.detener = function(){
+        console.log(`Deteniendo...${this.modelo}`);
+    }
+}
+
+let auto1 = new coche("Renault", "Kangoo", 2010);
+auto1.arrancar();
+
+// EJERCICIO 22
+const Personitax = {
+    nombrex : "Thiago",
+    edadx : 20,
+    saludarx(){
+        console.log(`Hola, soy ${this.nombrex}`)
+    }
+};
+
+// EJERCICIO 23
+function carrito(nombre){
+    this.nombre = nombre;
+
+    this.agregar = function(){
+        console.log(`${this.nombre} fue agregado.`);
+    }
+}
+let carro = new carrito("Mario");
+carro.agregar();
+
+// EJERCICIO 24
+function ordenarNumeros(a = []){
+    return a.sort((num1, num2) => num1 - num2);
+}
+
+let numerosArr = [1,6,2,8,9,3,4,0,1,2,7,80,20,54,21];
+console.log(ordenarNumeros(numerosArr));
+
+function eliminarEstudiante(a = [], b){
+    return a.filter(Estudiante => Estudiante.nombre != b);
+}
+
+console.log(arregloEstudiantes); // ANTES DE ELIMINAR
+console.log(eliminarEstudiante(arregloEstudiantes, "Mariano")); // DESPUES DE ELIMINAR
